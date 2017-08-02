180f66d01aed712eed2bacd7344c2935db07a31a
hellflame <hellflamedly@gmail.com>
2017-08-02 10:59:22 +0800

    detail

--- meta.js
+++ meta.js
@@ -96,5 +96,5 @@ module.exports = {
     "test/e2e/**/*": "e2e",
     "project/src/router/**/*": "router"
   },
-  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack\nAdjustment Detail can be found at https://github.com/hellflame/webpack-tornado"
 };
