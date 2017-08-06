abd500b0923b003b5578c0513550d7afc712bb00
hellflame <hellflamedly@gmail.com>
2017-08-06 14:28:21 +0800

    logging support

--- template/run.py
+++ template/run.py
@@ -1,5 +1,6 @@
 import tornado.ioloop as ioloop
 import tornado.web as web
+import logging
 import sys
 from project.service import routes
 from project.config import settings, DEBUG
@@ -13,8 +14,17 @@ if __name__ == '__main__':
         port = int(sys.argv[1])
     if DEBUG:
         address = '0.0.0.0'
+        logging.basicConfig(level=logging.DEBUG)
     else:
+        # running at localhost and different ports to use nginx for load balancing
         address = '127.0.0.1'
+
+        """
+            better to log into sys.stdout,
+            and use `supervisor` or some other tools to redirect to safe file
+        """
+        logging.basicConfig(level=logging.WARNING)
+
     print("app runs&reload at http://{}:{}".format(address, port))
     app.listen(port, address=address)
     ioloop.IOLoop.current().start()
